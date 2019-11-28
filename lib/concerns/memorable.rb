9f27c4a184f7e6ac19d4f850eacce9b07b4f7b55
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-28 20:36:45 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,17 @@
 module Memorable
-  #module ClassMethods
+ module ClassMethods
   def reset_all
     self.all.clear
   end
   
   def count
     self.all.length
+   end
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.call << self
+    end
   end
 end
